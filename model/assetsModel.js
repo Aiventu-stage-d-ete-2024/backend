63454e7ba0bc4c052073000b8e59c471d6f2a2b1
MarsaouiMariem <marsaoui81@gmail.com>
2024-06-19 10:20:13 +0100

    AssetsModel+assetsRoutes+authMiddleware

---
+++ model/assetsModel.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+const {Schema,model,Types}=mongoose;
+
+const assetsSchema = new Schema({
+    ObjectID: String,
+    Name: String,
+    Parent: String,
+    Children: String,
+    AssetType: String,
+    Manufacturer: String,
+    Model: String,
+    CustomerAccount: String,
+    Criticality: String,
+    FunctionalLocation: String,
+    CurrentLifecycleState: String,
+});
+
+export default model('Assets', assetsSchema);
