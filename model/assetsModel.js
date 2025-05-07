a5d569b487bcfe661b2898a74c827c6a4e58589e
Marsaoui Mariem <marsaoui81@gmail.com>
2025-05-07 14:33:18 +0100

    Asset sync

--- model/assetsModel.js
+++ model/assetsModel.js
@@ -13,6 +13,16 @@ const assetsSchema = new Schema({
     Criticality: Number,
     FunctionalLocation: String,
     CurrentLifecycleState: String,
+    lastModified: { type: Date, default: Date.now, index: true },
+    version: { type: Number, default: 1 }
+}, {
+    timestamps: true
+});
+
+assetsSchema.pre('save', function(next) {
+    this.lastModified = Date.now();
+    this.version += 1;
+    next();
 });
 
 export default model('Assets', assetsSchema);
--- route/assetsRoute.js
+++ route/assetsRoute.js
@@ -5,17 +5,19 @@ import { getAllAssets,
          getAssetByAssetID, 
          updateAsset,
          deleteAsset,
-         searchAssets} from '../controller/assetsController.js';
+         searchAssets,
+         syncAssets,
+         getModifiedAssets,} from '../controller/assetsController.js';
 
 const router = express.Router();
 
 router.get('/search', searchAssets);
+router.get('/modified', getModifiedAssets);
 router.get('/', getAllAssets);
+router.post('/sync', syncAssets);
 router.post('/', createAsset);
 router.get('/:AssetID', getAssetByAssetID);
 router.put('/:id', updateAsset);
 router.delete('/:id', deleteAsset);
-
-
 
 export default router;
